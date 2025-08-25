3a95c13c77329ab73453ccf429dd4156e2e1e2c1
iLa <nabilaakbars1@gmail.com>
2025-08-25 09:44:32 +0800

    tambah codingan

---
+++ tugas1.py
@@ -0,0 +1,17 @@
+def faktorial(n):
+    if n < 0:
+        return "Input harus bilangan bulat non-negatif."
+    elif n == 0 or n == 1:
+        return 1
+    else:
+        result = 1
+        for i in range(2, n + 1):
+            result *= i
+        return result
+
+try:
+    user_input = int(input("Masukkan bilangan bulat non-negatif: "))
+    result = faktorial(user_input)
+    print(f"Faktorial dari {user_input} adalah: {result}")
+except ValueError:
+    print("Input tidak valid. Harap masukkan bilangan bulat.")
