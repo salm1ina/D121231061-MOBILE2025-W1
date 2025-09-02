196452706d188ad01b06ec6fc0135ce9b7d05a6b
iLa <nabilaakbars1@gmail.com>
2025-09-02 11:01:59 +0800

    run

---
+++ Week 2/tugas2 (run).dart
@@ -0,0 +1,11 @@
+import 'tugas2 (fibo).dart';
+import 'tugas2 (fak).dart';
+import 'tugas2 (AB).dart';
+
+void main() {
+  int number = 100;
+
+  print("Hasil Fibonaccinya dari $number adalah = ${fibonacci(number)}");
+  print("Hasil Faktorialnya dari $number adalah = ${faktorial(number)}");
+  print("Hasil Angka Biner nya dari $number adalah = ${AngkaBine(number)}");
+}
