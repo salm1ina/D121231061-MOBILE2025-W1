ff9a80cf26483bea0768daede50f0e0fc0754b03
iLa <nabilaakbars1@gmail.com>
2025-09-02 10:47:12 +0800

    angka biner

---
+++ Week 2/tugas2 (AB).dart
@@ -0,0 +1,13 @@
+String AngkaBine(int n) {
+  if (n == 0) {
+    return "0";
+  }
+
+String BinerString = "";
+while (n > 0) {
+  int sisa = n % 2;
+  BinerString = sisa.toString() + BinerString;
+  n = n~/ 2;
+}
+  return BinerString;
+}
